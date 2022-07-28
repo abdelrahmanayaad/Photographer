c1dcf1d0eed71b0be789419767b0d055eda94ae0
abdelrahmanayaad74 <abdelrahmanayad74@gmail.com>
2022-07-28 15:29:56 +0200

    react-native-vector-icons

--- App.js
+++ App.js
@@ -3,12 +3,22 @@ import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import {COLORS, FONTS, ICONS, RADIUS, PADDING} from './src/constants';
 import GeneralButton from './src/components/GeneralButton';
 import Input from './src/components/Input';
+import Entypo from 'react-native-vector-icons/Entypo';
 const {width, height} = Dimensions.get('window');
 export class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textStyle}>Hello World !</Text>
+        <Entypo name="behance" color="red" size={25} />
+        {/* <GeneralButton
+          title="Sign up"
+          bgcolor={COLORS.primary}
+          activeOpacity={0.7}
+          onPress={() => alert('Hello')}
+        /> */}
+        {/* <Input placeholder="First Name" style={{width: 150}} />
+        <Input placeholder="Last Name" style={{width: 150}} /> */}
       </View>
     );
   }
