aa3f35682be07f40fb2091f905cb3d3abda4e29b
abdelrahmanayaad74 <abdelrahmanayad74@gmail.com>
2022-07-28 19:42:35 +0200

    Forget Password Screen

--- App.js
+++ App.js
@@ -4,35 +4,12 @@ import {COLORS, FONTS, ICONS, RADIUS, PADDING} from './src/constants';
 import GeneralButton from './src/components/GeneralButton';
 import Input from './src/components/Input';
 import Entypo from 'react-native-vector-icons/Entypo';
+import {ForgetPassword} from './src/screens';
 const {width, height} = Dimensions.get('window');
 export class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.textStyle}>Hello World !</Text>
-        <Entypo name="behance" color="red" size={25} />
-        {/* <GeneralButton
-          title="Sign up"
-          bgcolor={COLORS.primary}
-          activeOpacity={0.7}
-          onPress={() => alert('Hello')}
-        /> */}
-        {/* <Input placeholder="First Name" style={{width: 150}} />
-        <Input placeholder="Last Name" style={{width: 150}} /> */}
-      </View>
-    );
+    return <ForgetPassword />;
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  textStyle: {
-    fontSize: FONTS.h1,
-    color: COLORS.black,
-    fontWeight: 'bold',
-  },
-});
+const styles = StyleSheet.create({});
 export default App;
--- src/components/GeneralButton.js
+++ src/components/GeneralButton.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {RADIUS, COLORS, FONTS} from '../constants';
+import {RFValue} from 'react-native-responsive-fontsize';
 const {width, height} = Dimensions.get('window');
 export class GeneralButton extends Component {
   constructor(props) {
@@ -26,15 +27,17 @@ export class GeneralButton extends Component {
 }
 const styles = StyleSheet.create({
   container: {
-    width: width * 0.8,
-    height: height * 0.075,
-    borderRadius: RADIUS.xsRadius,
+    // width: width * 0.7,
+    width: RFValue(230),
+    // height: height * 0.065 >= 45 ? height * 0.065 : 40,
+    height: RFValue(45),
+    borderRadius: RFValue(RADIUS.xsRadius),
     justifyContent: 'center',
     alignItems: 'center',
   },
   titleStyle: {
     color: COLORS.white,
-    fontSize: FONTS.h3,
+    fontSize: RFValue(FONTS.h4),
     fontWeight: 'bold',
   },
 });
