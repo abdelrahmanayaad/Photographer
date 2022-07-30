8400a49d8ba4efc6126fe6cd93eec19957c5e0cf
MohamedMat-hana <mathanamohamed9@gmail.com>
2022-07-30 03:26:13 +0200

    sign up update

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import {COLORS, FONTS, ICONS, RADIUS, PADDING} from './src/constants';
-import GeneralButton from './src/components/GeneralButton';
-import Input from './src/components/Input';
-import Entypo from 'react-native-vector-icons/Entypo';
-import {ForgetPassword} from './src/screens';
+// import GeneralButton from './src/components/GeneralButton';
+// import Input from './src/components/Input';
+// import Entypo from 'react-native-vector-icons/Entypo';
+// import {ForgetPassword} from './src/screens';
+import {Signup} from './src/screens';
+
 const {width, height} = Dimensions.get('window');
 export class App extends Component {
   render() {
-    return <ForgetPassword />;
+    return <Signup />;
   }
 }
 const styles = StyleSheet.create({});
