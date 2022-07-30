f25b5680dd5d9b14dfe50d29050fe4ba25f35069
abdllrhmanelshazly <abdllrhmanelshazly@gmial.com>
2022-07-30 22:49:33 +0200

    add

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
-import {COLORS, FONTS, ICONS, RADIUS, PADDING} from './src/constants';
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { COLORS, FONTS, ICONS, RADIUS, PADDING } from './src/constants';
 import GeneralButton from './src/components/GeneralButton';
 import Input from './src/components/Input';
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -11,10 +11,10 @@ import {
   Signup,
   Login,
 } from './src/screens';
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 export class App extends Component {
   render() {
-    return <Login />;
+    return <Verification />;
   }
 }
 const styles = StyleSheet.create({});
