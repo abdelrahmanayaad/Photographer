2d69ac085cd1e5222740167e0ed16d69c4cc62e5
MohamedMat-hana <mathanamohamed9@gmail.com>
2022-07-30 16:20:02 +0200

    message

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import {COLORS, FONTS, ICONS, RADIUS, PADDING} from './src/constants';
 import GeneralButton from './src/components/GeneralButton';
 import Input from './src/components/Input';
 import Entypo from 'react-native-vector-icons/Entypo';
-import { ForgetPassword, Verification,ChangePassword } from './src/screens';
+import { ForgetPassword, Verification,ChangePassword,Signup } from './src/screens';
 const { width, height } = Dimensions.get('window');
 export class App extends Component {
   render() {
-    return <ChangePassword />;
+    return <Signup />;
 
   }
 }
